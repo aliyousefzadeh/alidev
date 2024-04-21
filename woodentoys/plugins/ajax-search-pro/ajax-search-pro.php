e3cf6e7349a397521ad083b9c814d6c85b2c62d7
Ali <ali.yousefzadeh@gmail.com>
2024-04-21 16:02:33 +0300

    add wordpress

---
+++ woodentoys/plugins/ajax-search-pro/ajax-search-pro.php
@@ -0,0 +1,68 @@
+<?php
+/*
+Plugin Name: Ajax Search Pro
+Plugin URI: https://temifa.ir/product/ajax-search-pro-%d8%a7%d9%81%d8%b2%d9%88%d9%86%d9%87/
+Description: قوی ترین افزونه سرج در جهان - ارائه شده توسط تمیفا - برای آموزش افزونه به وبسایت تمیفا مراجعه کنید
+Version: 4.16.3
+Author: برای دریافت آخرین بروز رسانی از سرور تمیفا کلیک کنید
+Author URI: https://temifa.ir/product/ajax-search-pro-%d8%a7%d9%81%d8%b2%d9%88%d9%86%d9%87/
+Text Domain: ajax-search-pro
+Domain Path: /languages/
+*/
+
+/* Prevent direct access */
+defined('ABSPATH') or die("You can't access this file directly.");
+
+define('ASP_FILE', __FILE__);
+define('ASP_PATH', plugin_dir_path(__FILE__));
+define('ASP_CSS_PATH', plugin_dir_path(__FILE__)."/css/");
+define('ASP_INCLUDES_PATH', plugin_dir_path(__FILE__)."/includes/");
+define('ASP_CLASSES_PATH', plugin_dir_path(__FILE__)."/includes/classes/");
+define('ASP_EXTERNALS_PATH', plugin_dir_path(__FILE__)."/includes/externals/");
+define('ASP_FUNCTIONS_PATH', plugin_dir_path(__FILE__)."/includes/functions/");
+define('ASP_TT_CACHE_PATH', plugin_dir_path(__FILE__)."/includes/cache/");
+define('ASP_DIR', 'ajax-search-pro');
+define('ASP_PLUGIN_NAME', 'ajax-search-pro/ajax-search-pro.php');
+define(
+    'ASP_URL',
+    strpos(home_url('/'), 'https://') !== false || strpos(plugin_dir_url(__FILE__), 'https://') !== false ?
+    str_replace('http://', 'https://', plugin_dir_url(__FILE__)) : plugin_dir_url(__FILE__)
+);
+define('ASP_URL_NP',  str_replace(array("http://", "https://"), "//", plugin_dir_url(__FILE__)));
+define('ASP_CURR_VER', 4988);
+define('ASP_CURR_VER_STRING', "4.16.3");
+define('ASP_PLUGIN_SLUG', plugin_basename(__FILE__) );
+define('ASP_DEBUG', 0);
+define('ASP_DEMO', get_option('wd_asp_demo', 0) );
+// The one and most important global
+global $wd_asp;
+
+require_once(ASP_CLASSES_PATH . "core/core.inc.php");
+/**
+ *  wd_asp()->_prefix      => correct DB prefix for ASP databases
+ *  wd_asp()->tables       => table names
+ *  wd_asp()->db           => DB manager
+ *  wd_asp()->options      => array of default option arrays
+ *  wd_asp()->o            => alias of wd_asp()->options
+ *  wd_asp()->instances    => array of search instances and data
+ *  wd_asp()->init         => initialization object
+ *  wd_asp()->manager      => main manager object
+ *  wd_asp()->upload_dir   => the upload directory name
+ *  wd_asp()->upload_path  => the upload path (with backslash)
+ */
+$wd_asp = new WD_ASP_Globals();
+
+if ( !function_exists("wd_asp") ) {
+    /**
+     * Easy access of the global variable reference
+     *
+     * @return WD_ASP_Globals
+     */
+    function wd_asp() {
+        global $wd_asp;
+        return $wd_asp;
+    }
+}
+
+// Initialize the plugin
+$wd_asp->manager = WD_ASP_Manager::getInstance();
---
+++ woodentoys/plugins/mailpoet/lib/EmailEditor/Engine/Renderer/Renderer.php
@@ -0,0 +1,120 @@
+<?php declare(strict_types = 1);
+
+namespace MailPoet\EmailEditor\Engine\Renderer;
+
+if (!defined('ABSPATH')) exit;
+
+
+use MailPoet\EmailEditor\Engine\Renderer\ContentRenderer\ContentRenderer;
+use MailPoet\EmailEditor\Engine\SettingsController;
+use MailPoet\Util\pQuery\DomNode;
+use MailPoetVendor\Html2Text\Html2Text;
+
+class Renderer {
+  private \MailPoetVendor\CSS $cssInliner;
+
+  private SettingsController $settingsController;
+
+  private ContentRenderer $contentRenderer;
+
+  const TEMPLATE_FILE = 'template.html';
+  const TEMPLATE_STYLES_FILE = 'template.css';
+
+  /**
+   * @param \MailPoetVendor\CSS $cssInliner
+   */
+  public function __construct(
+    \MailPoetVendor\CSS $cssInliner,
+    SettingsController $settingsController,
+    ContentRenderer $contentRenderer
+  ) {
+    $this->cssInliner = $cssInliner;
+    $this->settingsController = $settingsController;
+    $this->contentRenderer = $contentRenderer;
+  }
+
+  public function render(\WP_Post $post, string $subject, string $preHeader, string $language, $metaRobots = ''): array {
+    $layout = $this->settingsController->getLayout();
+    $themeStyles = $this->settingsController->getEmailStyles();
+    $padding = $themeStyles['spacing']['padding'];
+
+    $contentBackground = $themeStyles['color']['background']['content'];
+    $layoutBackground = $themeStyles['color']['background']['layout'];
+    $contentFontFamily = $themeStyles['typography']['fontFamily'];
+    $renderedBody = $this->contentRenderer->render($post);
+
+    $styles = (string)file_get_contents(dirname(__FILE__) . '/' . self::TEMPLATE_STYLES_FILE);
+    $styles = apply_filters('mailpoet_email_renderer_styles', $styles, $post);
+
+    $template = (string)file_get_contents(dirname(__FILE__) . '/' . self::TEMPLATE_FILE);
+
+    // Replace style settings placeholders with values
+    $template = str_replace(
+      ['{{width}}', '{{layout_background}}', '{{content_background}}', '{{content_font_family}}', '{{padding_top}}', '{{padding_right}}', '{{padding_bottom}}', '{{padding_left}}'],
+      [$layout['contentSize'], $layoutBackground, $contentBackground, $contentFontFamily, $padding['top'], $padding['right'], $padding['bottom'], $padding['left']],
+      $template
+    );
+
+    /**
+     * Replace template variables
+     * {{email_language}}
+     * {{email_subject}}
+     * {{email_meta_robots}}
+     * {{email_template_styles}}
+     * {{email_preheader}}
+     * {{email_body}}
+     */
+    $templateWithContents = $this->injectContentIntoTemplate(
+      $template,
+      [
+        $language,
+        esc_html($subject),
+        $metaRobots,
+        $styles,
+        esc_html($preHeader),
+        $renderedBody,
+      ]
+    );
+
+    $templateWithContentsDom = $this->inlineCSSStyles($templateWithContents);
+    $templateWithContents = $this->postProcessTemplate($templateWithContentsDom);
+    return [
+      'html' => $templateWithContents,
+      'text' => $this->renderTextVersion($templateWithContents),
+    ];
+  }
+
+  private function injectContentIntoTemplate($template, array $content) {
+    return preg_replace_callback('/{{\w+}}/', function ($matches) use (&$content) {
+      return array_shift($content);
+    }, $template);
+  }
+
+  /**
+   * @param string $template
+   * @return DomNode
+   */
+  private function inlineCSSStyles($template) {
+    return $this->cssInliner->inlineCSS($template);
+  }
+
+  /**
+   * @param string $template
+   * @return string
+   */
+  private function renderTextVersion($template) {
+    $template = (mb_detect_encoding($template, 'UTF-8', true)) ? $template : mb_convert_encoding($template, 'UTF-8', mb_list_encodings());
+    return @Html2Text::convert($template);
+  }
+
+  /**
+   * @param DomNode $templateDom
+   * @return string
+   */
+  private function postProcessTemplate(DomNode $templateDom) {
+    // because tburry/pquery contains a bug and replaces the opening non mso condition incorrectly we have to replace the opening tag with correct value
+    $template = $templateDom->__toString();
+    $template = str_replace('<!--[if !mso]><![endif]-->', '<!--[if !mso]><!-- -->', $template);
+    return $template;
+  }
+}
